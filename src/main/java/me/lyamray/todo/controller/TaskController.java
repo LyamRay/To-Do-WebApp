3e966c5a0d789db1caa93076f4ddc270bfc0d68c
LyamRay <lyam.ray410@gmail.com>
2025-08-29 03:33:40 +0200

    Updated.

--- src/main/java/me/lyamray/todo/controller/TaskController.java
+++ src/main/java/me/lyamray/todo/controller/TaskController.java
@@ -9,7 +9,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/tasks")
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "http://localhost:3000") //using ngrok you can create a free link so that users can acces it not only from localhost, place that also here!
 public class TaskController {
 
     private final TaskService service = new TaskService();
